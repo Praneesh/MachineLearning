9cc884394005ec0e0007de4b4bfc81629fdf96c2
Praneesh Kumar <pranuvitmsse05@gmail.com>
2017-10-29 18:15:54 +0530

    Implemented cost function to compute regularized costs.

--- Stanford-ML/Assignments/week3-machine-learning-ex2/ex2/costFunctionReg.m
+++ Stanford-ML/Assignments/week3-machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,11 +17,33 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[J, grad] = costFunction(theta, X, y);
 
+% Calcluting Regularization Term
+  theta_square_summation = 0.000;
+  for col=2:length(theta),  % theta(1) should not be regularized as it would be set to 1 always
+    theta_square_summation = theta_square_summation + (theta(col)) ** 2;    
+  end   
+  regularizationTerm = (lambda/(2*m))* theta_square_summation;
+  
+  J = J + regularizationTerm;
 
-
-
-
+% Step 3: Calucalte the Gradient    
+ 
+  % Regularized Gradient
+  for row=2:length(grad),
+      grad(row) = grad(row) + (lambda/m)*theta(row);
+  end
+  
 % =============================================================
 
 end
+
+
+
+
+
+
+
+
+
