1d2715564b6b53c9d0645f4ceb7aa8e31723f498
Praneesh Kumar <pranuvitmsse05@gmail.com>
2017-10-29 15:03:28 +0530

    Implemented costfunction calculation and gradient descent for the given data set.

--- Stanford-ML/Assignments/week3-machine-learning-ex2/ex2/costFunction.m
+++ Stanford-ML/Assignments/week3-machine-learning-ex2/ex2/costFunction.m
@@ -20,13 +20,36 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% Step 1: Calulate sigmoid function of all the values in data set
+  % Calculating - theta' * X for all the values and involking sigmoid function
+    htheta = zeros(m, 1);
+    for row=1:m,
+      feature_row = X(row,:); % Get each row in the X
+      x = feature_row'; % Transpose the x
+      z = theta' * x; 
+      htheta(row) = sigmoid(z);
+    end
 
-
-
-
-
-
-
+    
+% Step 2: Calucalte the Cost Function
+  summation = 0.00000;
+  for row=1:m,
+     acceptedStatus = y(row); % Gives if it is 1 or 0 as per the dataset
+     p_one = -(acceptedStatus) * log(htheta(row));
+     p_zero = (1-acceptedStatus) * log(1-htheta(row));
+     summation = summation + (p_one - p_zero);     
+  end
+  J = summation/m;
+  
+% Step 3: Calucalte the Gradient      
+  for feature_col=1:length(grad),
+    grad_summation = 0.00000;
+    for row=1:m,
+       pre_summation = (htheta(row) - y(row))* X(row, feature_col);
+       grad_summation = grad_summation + pre_summation;
+    end
+    grad(feature_col) = grad_summation/m;
+  end  
 % =============================================================
 
 end
